f83d3c58dd0cf0d21f22bd7eadebd3bdf8a80307
Peabaa <mdpastor14@gmail.com>
2025-05-15 03:59:57 +0800

    started landing page header

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <LandingPage />
+</template>
+
+<script>
+import LandingPage from './components/LandingPage.vue'
+
+export default {
+  components: {
+    LandingPage
+  }
+}
+</script>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class="bg-[#6EA1AA] shadow-md px-7 py-5">
+        <p class="font-sofia text-[48px] text-white text-shadow-lg/30">SkillSwap</p>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      isMenuOpen: false
+    }
+  }
+}
+</script>
